00b58737b2868a0b9cb4176a9f07e1e3c83783dd
JesCar-Gar <jesusalexandercara07@gmail.com>
2026-02-27 17:01:12 -0400

    base del proyecto

--- cgi-bin/reporte.py
+++ cgi-bin/reporte.py
@@ -1,12 +1,11 @@
-import pymysql
-import cgi
-import cgitb
-
+import pymysql, cgi, cgitb, os
+cgitb.enable()
 print("Content-Type: text/html\n")
 
-oyente = form.postvalue("oyente")
-artista= form.postvalue("artista")
-cancion = form.postvalue("cancion")
+form = cgi.FieldStorage()
+oyente = form.getvalue("oyente")
+artista = form.getvalue("artista")
+cancion = form.getvalue("cancion")
 
 if not oyente or not cancion:
     print('<meta http-equiv="refresh" content="2;url=/">')
@@ -21,17 +20,18 @@ else:
         )
         cur = conn.cursor()
         cur.execute(
-            "INSERT INTO peticiones (oyente, artista, cancion_artista) VALUES (%s, %s)",
-            (oyente,artista, cancion)
+            "INSERT INTO peticiones (oyente, artista, cancion_artista) VALUES (%s, %s, %s)",
+            (oyente, artista, cancion)
         )
         conn.commit()
         cur.close()
         conn.close()
         
         print('<meta http-equiv="refresh" content="3;url=/">')
-        print(f'<h3>✓ ¡Gracias {oyente}! Tu petición fue guardada</h3>')
+        print(f'<h3>Gracias {oyente}! Tu petición fue guardada</h3>')
         print(f'<p>Artista: {artista}</p>')
-        print(f'<p>Canción: {cancion}</p>')
+        print(f'<p>Cancion: {cancion}</p>')
+        
     except Exception as e:
-        print('<h3>✗ Error al guardar</h3>')
+        print('<h3>X Error al guardar</h3>')
         print('<p>Intenta de nuevo</p>')
